e3b91928d69728e88e40685110a6ef8253a4a038
oneraner <oneraner@gmail.com>
2022-06-04 23:39:00 +0800

    add comments

---
+++ api/comment.ts
@@ -0,0 +1,46 @@
+import axios from "../utils/axiosConfig";
+import Swal from "sweetalert2";
+
+const path = "/api/comments";
+let token: string | null = "";
+if (typeof window !== "undefined") {
+  token = localStorage.getItem("token");
+}
+
+interface AddCommentParam {
+  post_id?: string;
+  content: string;
+}
+
+export async function fetchGetComment(postId?: string) {
+  return await axios
+    .get(`${path}/${postId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(response => response.data.data)
+    .catch(error => {
+      console.log(error);
+      return [];
+    });
+}
+
+export async function fetchAddComment(data: AddCommentParam) {
+  return await axios
+    .post(path, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(res => res)
+    .catch(error => {
+      console.log(error);
+      Swal.fire({
+        title: "Error!",
+        text: "新增評論失敗，請稍後再試",
+        icon: "error",
+        confirmButtonText: "我知道了",
+      });
+    });
+}
